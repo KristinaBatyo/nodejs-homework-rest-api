a261f14895759be7f99c9bfd752bb63df8a16693
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-06-10 09:44:50 +0300

    finish

--- models/contact.js
+++ models/contact.js
@@ -2,31 +2,43 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const {handleMongooseError} = require("../decorators")
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const emailRegexp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: emailRegexp,
+    },
+    phone: {
+      type: String,
+      required: true,
+      match: /^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    }
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    match: /^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-}, { versionKey: false, timestamps: true });
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleMongooseError)
 
 const contactsAddSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
   phone: Joi.string()
     .pattern(/^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/)
     .required(),
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,30 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const contactsController = require("../../controllers/contacts-controllers");
-const {schemas} = require("../../models/contact");
+const { schemas } = require("../../models/contact");
 
 const { validateBody } = require("../../decorators");
+const { authenticate, isValidId } = require("../../middlewares");
+router.get("/", authenticate, contactsController.getAllContacts);
 
-router.get("/", contactsController.getAllContacts);
-
-router.get("/:contactId", contactsController.getContactsById);
+router.get("/:contactId", authenticate, isValidId, contactsController.getContactsById);
 
 router.post(
   "/",
+  authenticate,
   validateBody(schemas.contactsAddSchema),
   contactsController.addContacts
 );
 
-router.delete("/:contactId", contactsController.deleteContactsById);
+router.delete(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  contactsController.deleteContactsById
+);
 
 router.put(
   "/:contactId",
+  authenticate,
+  isValidId,
   validateBody(schemas.contactsAddSchema),
   contactsController.updateContactsById
 );
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
+  isValidId,
   validateBody(schemas.updateFavoriteSchema),
   contactsController.updateFavoriteById
 );
