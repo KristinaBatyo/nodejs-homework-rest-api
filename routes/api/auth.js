035f416f04c2659a690538dd908901cb97260166
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-06-16 13:50:59 +0300

    finish

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ctrl = require("../../controllers/auth-controllers");
 
 const { validateBody } = require("../../decorators");
-const {authenticate} = require("../../middlewares")
+const {authenticate, upload} = require("../../middlewares")
 const { schemas } = require("../../models/user");
 
 const router = express.Router();
@@ -11,4 +11,6 @@ router.post("/register", validateBody(schemas.registeSchema), ctrl.register);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
+
 module.exports = router;
