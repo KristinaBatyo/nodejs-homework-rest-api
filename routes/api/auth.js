19e7bb04edd2c9d43c2268667119c787afa313a2
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2023-06-19 10:55:14 +0300

    fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,7 +12,7 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
-router.get("verify/:verificationToken", ctrl.verify);
+router.get("/verify/:verificationToken", ctrl.verify);
 router.post("/resend-verify-email", validateBody(schemas.emailSchema, ctrl.resendVerifyEmail));
 
 
